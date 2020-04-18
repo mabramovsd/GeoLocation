d6efa048ccd34605fd4e4627bab38324e6ba058b
Mikhail Abramov <beavisabra@yandex.ru>
2020-04-18 12:56:10 +0400

    Main commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Geo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "City";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "City";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 80);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "IpWhoIs";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(2, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 80);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "DaDaTa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(228, 29);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "46.226.227.20";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "IP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 215);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Geo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Authenticators;
+using System.Windows.Forms;
+
+namespace Geo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(
+                "http://free.ipwhois.io/json/" + textBox1.Text);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader sr = new StreamReader(stream);
+
+            string sReadData = sr.ReadToEnd();
+            response.Close();
+
+            dynamic d = JsonConvert.DeserializeObject(sReadData);
+            label1.Text = d.city;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RestClient client = new RestClient("https://suggestions.dadata.ru/suggestions/api/4_1/rs/iplocate/address?ip="
+                + textBox1.Text);
+            RestRequest request = new RestRequest(Method.GET);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("content-type", "application/x-www-form-urlencoded");
+            request.AddHeader("Authorization", "Token your_token");
+            IRestResponse response = client.Execute(request);
+
+            string stream = response.Content;
+
+            dynamic d = JsonConvert.DeserializeObject(stream);
+            label2.Text = d.location.value;
+        }
+    }
+}
